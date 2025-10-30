2af7db0707e2b3e8a2bc9859e46906fc66565948
trieuduc2407 <trieuduc2407@gmail.com>
2025-10-30 13:16:38 +0700

    fix: update import path for userController in userRoutes; ensure correct module resolution

--- server/routes/client/userRoutes.js
+++ server/routes/client/userRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 
-import * as userController from '../../controllers/userController.js'
+import * as userController from '../../controllers/client/userController.js'
 
 const router = express.Router()
 
 router.post('/find-or-create', userController.findOrCreateUser)
 
-export default router
+export { router }
